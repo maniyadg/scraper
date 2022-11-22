54860c29be9fa1f1e5dfdd75cffe06ae4eae8bca
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2022-11-22 14:25:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import React, { useState } from 'react';
 
 function App() {
+  const [productlist,setproductlist] = useState([])
+  const item = async () => {
+    const itemlist = await axios.get("http://localhost:303/Products");
+    setproductlist(itemlist.data)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4">
+          {productlist.map((items) => {
+            return (
+              <div className="card border-dark mb-3">
+                <div className="card-header">`${items.title}`</div>
+                <div className="card-body text-dark">
+                  <h5 className="card-title">Dark card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
